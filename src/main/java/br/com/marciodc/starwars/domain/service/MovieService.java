e529af8d9f040286603c3ed16d1fc977661956c3
Marcio <marciodc@gmail.com>
2024-04-29 16:53:21 -0300

    Comments

--- src/main/java/br/com/marciodc/starwars/domain/service/MovieService.java
+++ src/main/java/br/com/marciodc/starwars/domain/service/MovieService.java
@@ -14,16 +14,34 @@ public class MovieService implements MovieServicePort {
         this.movieRepositoryPort = movieRepositoryPort;
     }
 
+    /**
+        * Retrieves a list of all movies.
+        *
+        * @return a list of Movie objects representing all movies.
+        */
     @Override
     public List<Movie> allMovies() {
         return movieRepositoryPort.listMovies();
     }
 
+    /**
+        * Updates the opening crawl of a movie.
+        *
+        * @param id    the ID of the movie
+        * @param movie the updated movie object
+        * @return the updated movie object
+        */
     @Override
     public Movie updatOpeningCrawl(int id, Movie movie) {
         return movieRepositoryPort.updatOpeningCrawl(id, movie);
     }
 
+    /**
+        * Finds a movie by its episode ID.
+        *
+        * @param id the episode ID of the movie
+        * @return the movie with the specified episode ID, or null if not found
+        */
     @Override
     public Movie findByEpisodeId(int id) {
         return movieRepositoryPort.findByEpisodeId(id);
